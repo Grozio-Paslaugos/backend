29ca49229e718bb4e3371b1239c18dc933ce854b
kestulka <kgirdiunas0@gmail.com>
2024-05-30 12:44:02 +0300

    .

---
+++ Models/notificationModel.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const notificationSchema = new mongoose.Schema(
+  {
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("Notification", notificationSchema);
